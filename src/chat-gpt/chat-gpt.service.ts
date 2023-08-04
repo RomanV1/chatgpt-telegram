d3d345ff67cf79e5451ee8aa571a3f80658d292e
Roma <gnida2404@yandex.ru>
2023-08-04 08:53:46 +0300

    style

--- src/chat-gpt/chat-gpt.service.ts
+++ src/chat-gpt/chat-gpt.service.ts
@@ -12,7 +12,7 @@ export class ChatGPTService {
     async sendMessage(messages: ChatCompletionResponseMessage[]): Promise<string> {
         const { data } = await this.openAI.createChatCompletion({
             model: 'gpt-3.5-turbo',
-            messages: messages,
+            messages,
         });
 
         return data.choices[0].message.content;
